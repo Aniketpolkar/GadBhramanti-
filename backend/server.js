8741a9e229edfb79093a8578d1614184e47d279f
Aniket Polkar <aniketpolkar@gmail.com>
2025-08-30 16:12:05 +0530

    30/8 4:11

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,9 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-
+// , { useNewUrlParser: true, useUnifiedTopology: true }
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
